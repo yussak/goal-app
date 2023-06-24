026ffaf85c67946deb6004df39d82be588c5aff1
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-06-24 12:49:27 +0900

    feat: read InitDB()

--- backend/main.go
+++ backend/main.go
@@ -5,9 +5,16 @@ import (
 	"net/http"
 
 	"github.com/YusukeSakuraba/goal-app/controller"
+	"github.com/YusukeSakuraba/goal-app/internal/db"
 )
 
 func main() {
+	// 不具合防止のため明示的に呼び出す
+	_, err := db.InitDB()
+	if err != nil {
+		log.Fatalf("Error initializing database: %v", err)
+	}
+
 	http.HandleFunc("/goal", controller.AddGoal)
 	
 	log.Fatal(http.ListenAndServe(":8080", nil))
