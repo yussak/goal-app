53f9577ca7b830666de6df08e13395383ab7ebbc
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-06-24 12:58:59 +0900

    feat: enable to add goal from form

--- backend/controller/goal_controller.go
+++ backend/controller/goal_controller.go
@@ -2,38 +2,48 @@ package controller
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/YusukeSakuraba/goal-app/internal/db"
+	"github.com/YusukeSakuraba/goal-app/model"
+	"github.com/oklog/ulid"
 )
 
-func AddGoal(w http.ResponseWriter, r *http.Request) {
+	func AddGoal(w http.ResponseWriter, r *http.Request) {
 	// TODO:ここに書くべきじゃないと思うので確認
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Contetn-Type", "application/json")
 
-	fmt.Println("titleee: ",r.FormValue("title"))
-	fmt.Println("texttt: ",r.FormValue("text"))
-	// err := r.ParseForm()
+	// デバッグ用に残す
+	// fmt.Println("goal title is: ",r.FormValue("title"))
+	// fmt.Println("goal text is: ",r.FormValue("text"))
 
-	// if err != nil {
-	// 	return nil, nil
-	// }
+	err := r.ParseForm()
 
-	// t := time.Now()
-	// entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
-	// id := ulid.MustNew(ulid.Timestamp(t), entropy)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-	// req := Goal {
-	// 	Id:     id.String(),
-	// 	Title:   r.FormValue("title"),
-	// 	Text: r.FormValue("text"),
-	// }
+	t := time.Now()
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
+	id := ulid.MustNew(ulid.Timestamp(t), entropy)
 
-	// sql := `INSERT INTO Goal(id, title, text) VALUES(?, ?, ?)`
-	// result, err := Db.Exec(sql, req.Id, req.Title, req.Text)
+	req := model.Goal {
+		ID:     id.String(),
+		Title:   r.FormValue("title"),
+		Text: r.FormValue("text"),
+	}
 
-	// if err != nil {
-	// 	return result, err
-	// }
-	// return result, nil
+	sql := `INSERT INTO Goal(id, title, text) VALUES(?, ?, ?)`
+	result, err := db.DB.Exec(sql, req.ID, req.Title, req.Text)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Successfully inserted goal with ID %s", result)
 }
