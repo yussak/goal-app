cc03c9b54ee8f7b1b6ab40dcd9bb63e9b4a44527
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-02 12:02:35 +0900

    refactor: change order of import

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 import GoalForm from "@/components/form/GoalForm";
 
 export default function Home() {
