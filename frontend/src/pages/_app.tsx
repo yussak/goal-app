b75beebf2db8b79b4ac37b67eccdc5a39cdf4f5e
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-02 22:00:57 +0900

    feat: enable to switch language

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-}
+};
+
+export default appWithTranslation(MyApp);
--- frontend/src/pages/goals/index.tsx
+++ frontend/src/pages/goals/index.tsx
@@ -3,8 +3,11 @@ import axios from "axios";
 import GoalForm from "@/components/form/GoalForm";
 import GoalList from "@/components/GoalList";
 import { Goal } from "@/types";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function GoalIndex() {
+  const { t, i18n } = useTranslation();
   const [goals, setGoals] = useState<Goal[]>([]);
 
   const [title, setTitle] = useState<string>("");
@@ -49,10 +52,18 @@ export default function GoalIndex() {
       });
   };
 
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <>
       <div>
         <h2>目標を追加する</h2>
+        <p>{t("test")}</p>
+        {/* TODO:コンポーネントに切り出す（ヘッダーが良さそう） */}
+        <button onClick={() => changeLanguage("en")}>{t("english")}</button>
+        <button onClick={() => changeLanguage("ja")}>{t("japanese")}</button>
         <GoalForm
           setTitle={setTitle}
           setText={setText}
@@ -64,4 +75,13 @@ export default function GoalIndex() {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
