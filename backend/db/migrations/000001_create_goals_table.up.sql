42ab39c44533b7ceda6d41ea74730f3941731c42
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-03 22:54:36 +0900

    fix: fix goals table migration

--- backend/db/migrations/000001_create_goals_table.up.sql
+++ backend/db/migrations/000001_create_goals_table.up.sql
@@ -1,8 +1,5 @@
--- TODO:goalじゃなくgoalsが正しいと思うのでロールバックして変更予定
-CREATE TABLE IF NOT EXISTS goal (
-  -- TODO: 	id varchar(26) not null PRIMARY KEY,にロールバックして変更する
-  	id varchar(26) not null,
-    title varchar(50) not null,
-    -- TODO: varcharじゃなくTEXT型に変更予定
-    text varchar(100) not null
+CREATE TABLE IF NOT EXISTS goals (
+  id varchar(26) not null PRIMARY KEY,
+  title varchar(50) not null,
+  text TEXT not null
 );
