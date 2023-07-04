fbbc940c818ce8a49dfef2393f4f87ef3f2a4469
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-04 11:01:00 +0900

    fix: fix struct

--- backend/model/goal_comment.go
+++ backend/model/goal_comment.go
@@ -2,7 +2,7 @@ package model
 
 type GoalComment struct {
 	ID string `json:"id"`
-	GoalID string `json:goal_id`
+	GoalID string `json:"goal_id"`
 	Title string `json:"title"`
 	Text string `json:"text"`
 }
