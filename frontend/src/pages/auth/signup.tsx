cb601b7fa8daf94c1d2e1b40ab4580b9a8274951
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-07 02:12:01 +0900

    feat: create base of signup form

---
+++ frontend/src/pages/auth/signup.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await axios.post("http://localhost:8080/auth/signup", {
+      name,
+      email,
+      password,
+    });
+
+    console.log(res.data);
+  };
+
+  return (
+    <form onSubmit={register}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
