64ab4aa180d7e397a8596098b00a5cdd6f06b775
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-08 19:49:14 +0900

    feat: add login form

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
+import { UserContext } from "@/contexts/userContext";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login } = useContext(UserContext);
 
   const handleSumit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -12,8 +14,12 @@ export default function Login() {
       email,
       password,
     });
-
-    console.log(res.data);
+    // TODO: 必要なのは確定なのでその理由をコメント
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+      // Set the user state
+      login(res.data.user);
+    }
   };
 
   // TODO: バリデーション追加;
