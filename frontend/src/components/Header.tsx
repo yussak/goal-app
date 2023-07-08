c85b5ac32aff8d68e9203edc071d39bb747ca282
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-08 19:50:06 +0900

    feat: enable to switch display if login or not

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 import { LanguageSwitcher } from "./LanguageSwitcher";
+import { UserContext } from "@/contexts/userContext";
+import { useContext } from "react";
 
 export default function Header() {
+  const { user } = useContext(UserContext);
+
   return (
     <header className="header">
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/goals">Goals</Link>
       <LanguageSwitcher />
+      {user ? <p>{user.name} desu</p> : "ログインしてない"}
     </header>
   );
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,12 +2,23 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { appWithTranslation } from "next-i18next";
+import { UserContext } from "@/contexts/userContext";
+import { useState } from "react";
+import { User } from "@/types";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (userData: User) => {
+    setUser(userData);
+  };
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <UserContext.Provider value={{ user, login }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserContext.Provider>
   );
 };
 
