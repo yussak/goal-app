1d6dffe6157260cedeeabbb7bb301ea0b53c9ce1
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-09 14:55:14 +0900

    feat: enable to get users

---
+++ backend/controller/user_controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/YusukeSakuraba/goal-app/internal/db"
+	"github.com/YusukeSakuraba/goal-app/model"
+	"github.com/gin-gonic/gin"
+)
+
+func FetchUsers(c *gin.Context) {
+	users := []model.User{}
+	
+	rows, err := db.DB.Query("SELECT id, name FROM users")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user model.User
+		err = rows.Scan(&user.ID, &user.Name)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		users = append(users, user)
+	}
+
+	c.JSON(http.StatusOK, users)
+}
--- frontend/src/pages/users/index.tsx
+++ frontend/src/pages/users/index.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { User } from "@/types";
+
 export default function UserIndex() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/users"
+      );
+      setUsers(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h2>ユーザー一覧</h2>
-
-      {/* <ul>
+      <ul>
         {users.map((user, index) => {
           return (
             <li key={index}>
-              <p>user</p>
+              <p>id(デバッグ用): {user.id}</p>
+              <p>name: {user.name}</p>
             </li>
           );
         })}
-      </ul> */}
+      </ul>
     </>
   );
 }
