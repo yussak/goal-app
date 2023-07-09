8e65a5a4e95defcd3aef0fe79f8ec5189c789395
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-09 13:29:10 +0900

    docs: rewrite getGoals with try-catch

--- frontend/src/pages/goals/index.tsx
+++ frontend/src/pages/goals/index.tsx
@@ -33,17 +33,15 @@ export default function GoalIndex() {
     }
   };
 
-  // thenを使う場合はasync/awaitは不要ということ（理解できてないので調べる）
-  // try-catchだとasync awaitはいるのか。どちらにすべきか確認する
-  const getGoals = () => {
-    axios
-      .get(process.env.NEXT_PUBLIC_API_URL + "/goals")
-      .then(({ data }) => {
-        setGoals(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  const getGoals = async () => {
+    try {
+      const { data } = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/goals"
+      );
+      setGoals(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const deleteGoal = (id: string) => {
