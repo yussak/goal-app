b92370ec98d4459500f20c00e8406a8fd13ac909
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-17 19:14:15 +0900

    feat: prepare for getting user's goals

--- frontend/src/pages/users/[id].tsx
+++ frontend/src/pages/users/[id].tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { User } from "@/types";
+import { Goal, User } from "@/types";
+import GoalList from "@/components/GoalList";
 
 export default function UserDetail() {
   const [user, setUser] = useState<User | null>(null);
+  const [goals, setGoals] = useState<Goal[]>([]);
 
   const router = useRouter();
   const id = router.query.id;
@@ -25,14 +27,37 @@ export default function UserDetail() {
     }
   };
 
+  const getGoals = async () => {
+    try {
+      const { data } = await axios.get(
+        process.env.NEXT_PUBLIC_API_URL + "/goals"
+      );
+      setGoals(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteGoal = async (id: string) => {
+    try {
+      await axios.delete(process.env.NEXT_PUBLIC_API_URL + `/goal/${id}`);
+      await getGoals();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h2>ユーザー詳細</h2>
+      {/* TODO:ユーザー詳細分をコンポーネントに切り出す */}
       {user && (
         <>
           <p>name: {user.name}</p>
         </>
       )}
+      {/* TODO:そのユーザーのgoalだけ取得する */}
+      <GoalList goals={goals} onDelete={deleteGoal} />
     </>
   );
 }
