ff8a88a313183dbf2647d1effec470a4699435dc
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-21 23:58:04 +0900

    refactor: separate routers into router.go

--- backend/main.go
+++ backend/main.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/YusukeSakuraba/goal-app/controller"
 	"github.com/YusukeSakuraba/goal-app/internal/db"
+	"github.com/YusukeSakuraba/goal-app/router"
 )
 
 func main() {
@@ -21,24 +21,8 @@ func main() {
 
 	r.Use(corsMiddleware())
 	
-	r.POST("auth/signup", controller.Signup)
-	r.POST("auth/login", controller.Login)
-	r.POST("auth/decodeToken", controller.DecodeToken)
-
-	r.GET("/users", controller.FetchUsers)
-	r.GET("/users/:id/details", controller.FetchUserDetails)
-	r.GET("/users/:id/goals", controller.FetchUserGoals)
-
-	r.POST("/goal", controller.AddGoal)
-	r.GET("/goals", controller.FetchGoals)
-	r.DELETE("/goal/:id", controller.DeleteGoal)
-	r.GET("/goals/:id", controller.FetchGoalDetails)
-	r.PUT("/goals/edit/:id", controller.EditGoal)
+	router.SetupRouter(r)
 	
-	r.POST("/goals/:id/comments", controller.AddGoalComment)
-	r.GET("/goals/:id/comments", controller.FetchGoalComments)
-	r.DELETE("/goals/:goal_id/comments/:comment_id", controller.DeleteGoalComment)
-
 	r.Run(":8080")
 }
 
---
+++ backend/router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"github.com/YusukeSakuraba/goal-app/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(r *gin.Engine) {
+
+	r.POST("auth/signup", controller.Signup)
+	r.POST("auth/login", controller.Login)
+	r.POST("auth/decodeToken", controller.DecodeToken)
+
+	r.GET("/users", controller.FetchUsers)
+	r.GET("/users/:id/details", controller.FetchUserDetails)
+	r.GET("/users/:id/goals", controller.FetchUserGoals)
+
+	r.POST("/goal", controller.AddGoal)
+	r.GET("/goals", controller.FetchGoals)
+	r.DELETE("/goal/:id", controller.DeleteGoal)
+	r.GET("/goals/:id", controller.FetchGoalDetails)
+	r.PUT("/goals/edit/:id", controller.EditGoal)
+	
+	r.POST("/goals/:id/comments", controller.AddGoalComment)
+	r.GET("/goals/:id/comments", controller.FetchGoalComments)
+	r.DELETE("/goals/:goal_id/comments/:comment_id", controller.DeleteGoalComment)
+}
