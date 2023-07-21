6bb0399f2d0c4915afa8eae28ae85877ef21f99d
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-22 00:27:15 +0900

    feat: enable to switch sidebar by login status

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,18 +1,27 @@
+import { useUser } from "@/contexts/userContext";
 import Link from "next/link";
 
 export default function Sidebar() {
+  const { user } = useUser();
+
   return (
     <ul>
-      <li>
-        <Link href="/auth/signup">signup</Link>
-      </li>
-      <li>
-        <Link href="/auth/login">login</Link>
-      </li>
+      {!user ? (
+        <>
+          <li>
+            <Link href="/auth/signup">signup</Link>
+          </li>
+          <li>
+            <Link href="/auth/login">login</Link>
+          </li>
+          <p>非ログイン（デバッグ用）</p>
+        </>
+      ) : (
+        <p>ログイン済み（デバッグ用）</p>
+      )}
       <li>
         <Link href="/users">ユーザー一覧</Link>
       </li>
-      {/* TODO:ログインした時だけ自分のユーザー詳細ページへのリンク「マイページ」を追加する */}
     </ul>
   );
 }
