2df90151d47cf7fd91a59ab332131e13bdebf599
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-22 00:17:56 +0900

    refactor: remove useEffect

--- frontend/src/pages/goals/index.tsx
+++ frontend/src/pages/goals/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import GoalForm from "@/components/form/GoalForm";
 import GoalList from "@/components/GoalList";
