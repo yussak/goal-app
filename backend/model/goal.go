5fdf23f57cca5bd5266a7fa54d4c2f1edc034445
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-25 22:21:34 +0900

    feat: add column to struct

--- backend/model/goal.go
+++ backend/model/goal.go
@@ -1,9 +1,13 @@
 package model
 
+import "time"
+
 type Goal struct {
 	ID string `json:"id"`
 	UserID string `json:"user_id"`
 	Title string `json:"title"`
 	Text string `json:"text"`
 	ImageURL *string `json:"image_url"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
