3005b89beac01c163d7c979c94e8c9fdec0f3539
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-07-27 22:50:44 +0900

    style: add default icon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,7 +9,16 @@ export default function Header() {
     <header className="header">
       <LanguageSwitcher />
       <p>{user ? <span>{user.name} としてログイン</span> : "Not logged in"}</p>
-      <p>{user && <span>userid（デバッグ用）: {user.id}</span>}</p>
+      <p>
+        {user && (
+          <>
+            <span>userid（デバッグ用）: {user.id}</span>
+            {/* TODO:アイコン追加実装したらアイコン有無で切り替える */}
+            {/* 権利とか使用時の条件があると思うので問題ないものに変えるかその条件のことやる */}
+            <img src="/defaultIcon.png" alt="icon" />
+          </>
+        )}
+      </p>
     </header>
   );
 }
