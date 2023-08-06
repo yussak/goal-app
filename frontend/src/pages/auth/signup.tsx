91a4f45f9a47ee277d12313a3397d37da0d01adf
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-06 17:57:09 +0900

    refactor: remove unused code

--- frontend/src/pages/auth/signup.tsx
+++ frontend/src/pages/auth/signup.tsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
-import { checkAuth } from "@/utils/auth";
-import { NextPageContext } from "next";
-import { useLogin } from "@/hooks/useLogin";
-import { User } from "@/types";
 import SignupForm from "@/components/form/SignupForm";
 
 // TODO:登録時にログインする→ https://github.com/YusukeSakuraba/goal-app/issues/28 で対応
 // TODO: バリデーション追加→空欄（requiredでできてそうだが揃えたい）、文字数・形式
-export default function signup({ user: currentUser }: { user: User | null }) {
-  useLogin(currentUser);
-
+export default function signup() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -43,14 +37,4 @@ export default function signup({ user: currentUser }: { user: User | null }) {
       password={password}
     />
   );
-}
-
-export async function getServerSideProps(context: NextPageContext) {
-  const user = await checkAuth(context);
-
-  return {
-    props: {
-      user,
-    },
-  };
 }
