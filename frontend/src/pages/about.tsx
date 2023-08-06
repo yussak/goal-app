d7e233ad23eeff232880bca44a8a4b8c1bc5348f
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-06 14:13:58 +0900

    feat: add optional chaining

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,9 +1,13 @@
+import { useSession } from "next-auth/react";
+
 export default function About() {
+  const { data: session } = useSession();
+
   return (
     <>
-      <div>
-          about
-      </div>
+      {/* TODO:nameが空文字の時もfalsyになるのでHelloが出てこない→正しいんだがそもそもnameが空欄にならぬようバリデーション追加する */}
+      {session?.user?.name && <p>Hello, {session?.user?.name}</p>}
+      <div>about</div>
     </>
-  )
+  );
 }
