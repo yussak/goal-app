914bf580dc9f3f8c49241a9d0be15e0ab5a0751e
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-08-12 11:18:37 +0900

    docs: remove comment

--- frontend/src/pages/users/[id].tsx
+++ frontend/src/pages/users/[id].tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-// import axios from "axios";
 import { axios } from "@/utils/axios";
 import { Goal, User } from "@/types";
 import GoalList from "@/components/GoalList";
