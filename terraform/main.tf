d14fca918464410242cf81eda64e70fda34807a3
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-02 15:39:16 +0900

    feat: add frontend log group

--- terraform/main.tf
+++ terraform/main.tf
@@ -544,8 +544,13 @@ module "backend_sg" {
 
 # cloudwatch ログ
 # Fargateではホストサーバーにログインできず、コンテナのログを直接確認できない。なのでログで確認できるようにする
-resource "aws_cloudwatch_log_group" "for_ecs" {
+resource "aws_cloudwatch_log_group" "for_ecs_backend" {
   name              = "/ecs/backend"
+  retention_in_days = 180
+}
+
+resource "aws_cloudwatch_log_group" "for_ecs_frontend" {
+  name              = "/ecs/frontend"
   retention_in_days = 180
 }
 
