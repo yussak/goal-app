a74460694f8a49a6851e1f7d4d6eb45d71e09301
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-09 01:52:47 +0900

    feat: remove elasticache things

--- terraform/main.tf
+++ terraform/main.tf
@@ -918,64 +918,6 @@ module "mysql_sg" {
   cidr_blocks = [aws_vpc.example.cidr_block]
 }
 
-# ElastiCacheパラメータグループ
-resource "aws_elasticache_parameter_group" "example" {
-  name   = "example"
-  family = "redis5.0"
-
-  parameter {
-    name  = "cluster-enabled"
-    value = "no"
-  }
-}
-
-# ElastiCacheサブネットグループ
-# プライベートサブネットを指定し、異なるアベイラビリティゾーンのものを含める
-resource "aws_elasticache_subnet_group" "example" {
-  name       = "example"
-  subnet_ids = [aws_subnet.private_0.id, aws_subnet.private_1.id]
-}
-
-# ElastiCacheレプリケーショングループ
-# Redisサーバーを作成
-resource "aws_elasticache_replication_group" "example" {
-  replication_group_id = "example"
-
-  # 日本語入ってたらエラーになった
-  description = "Cluster Disabled"
-
-  engine         = "redis"
-  engine_version = "5.0.4"
-
-  num_cache_clusters = 3
-  node_type          = "cache.t3.medium"
-
-  snapshot_window          = "09:10-10:10"
-  snapshot_retention_limit = 7
-
-  maintenance_window = "mon:10:40-mon:11:40"
-
-  # フェイルオーバー；システムが停止した時に自動的に待機システムに切り替える仕組み
-  automatic_failover_enabled = true
-
-  port = 6379
-
-  apply_immediately = false
-
-  security_group_ids = [module.redis_sg.security_group_id]
-
-  parameter_group_name = aws_elasticache_parameter_group.example.name
-  subnet_group_name    = aws_elasticache_subnet_group.example.name
-}
-
-# ElastiCacheレプリケーショングループのsgの定義
-module "redis_sg" {
-  source      = "./security_group"
-  name        = "redis-sg"
-  vpc_id      = aws_vpc.example.id
-  port        = 6379
-  cidr_blocks = [aws_vpc.example.cidr_block]
-}
 
 # ECRリポジトリ
 resource "aws_ecr_repository" "example" {
