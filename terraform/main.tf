02881a9f7e165c60f541ac1da6f10517394bf5d8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-09 17:57:08 +0900

    feat: remove unused thing

--- terraform/main.tf
+++ terraform/main.tf
@@ -658,15 +658,15 @@ resource "aws_cloudwatch_log_group" "for_ecs_scheduled_tasks" {
 }
 
 # バッチ用タスク定義
-resource "aws_ecs_task_definition" "example_batch" {
-  family                   = "example-batch"
-  cpu                      = "256"
-  memory                   = "512"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  container_definitions    = file("./batch_container_definitions.json")
-  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
-}
+# resource "aws_ecs_task_definition" "example_batch" {
+#   family                   = "example-batch"
+#   cpu                      = "256"
+#   memory                   = "512"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   container_definitions    = file("./batch_container_definitions.json")
+#   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+# }
 
 # cloudwatchイベントからECSを起動するためのIAMロールを作成する
 module "ecs_events_role" {
@@ -682,31 +682,31 @@ data "aws_iam_policy" "ecs_events_role_policy" {
 }
 
 # ジョブの実行スケジュールを定義する
-resource "aws_cloudwatch_event_rule" "example_batch" {
-  name                = "example-batch"
-  description         = "実践TFで作成"
-  schedule_expression = "cron(*/2 * * * ? *)"
-}
+# resource "aws_cloudwatch_event_rule" "example_batch" {
+#   name                = "example-batch"
+#   description         = "実践TFで作成"
+#   schedule_expression = "cron(*/2 * * * ? *)"
+# }
 
 # cloudwatchイベントターゲットで実行対象のジョブを定義
-resource "aws_cloudwatch_event_target" "example_batch" {
-  target_id = "example-batch"
-  rule      = aws_cloudwatch_event_rule.example_batch.name
-  role_arn  = module.ecs_events_role.iam_role_arn
-  arn       = aws_ecs_cluster.example.arn
+# resource "aws_cloudwatch_event_target" "example_batch" {
+#   target_id = "example-batch"
+#   rule      = aws_cloudwatch_event_rule.example_batch.name
+#   role_arn  = module.ecs_events_role.iam_role_arn
+#   arn       = aws_ecs_cluster.example.arn
 
-  ecs_target {
-    launch_type         = "FARGATE"
-    task_count          = 1
-    platform_version    = "1.3.0"
-    task_definition_arn = aws_ecs_task_definition.example_batch.arn
+#   ecs_target {
+#     launch_type         = "FARGATE"
+#     task_count          = 1
+#     platform_version    = "1.3.0"
+#     task_definition_arn = aws_ecs_task_definition.example_batch.arn
 
-    network_configuration {
-      assign_public_ip = "false"
-      subnets          = [aws_subnet.private_0.id]
-    }
-  }
-}
+#     network_configuration {
+#       assign_public_ip = "false"
+#       subnets          = [aws_subnet.private_0.id]
+#     }
+#   }
+# }
 
 # カスタマーキー
 resource "aws_kms_key" "example" {
