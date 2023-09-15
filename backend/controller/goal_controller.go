a7ad81a22f9181d673a5634cbbf90954c96a3d27
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-15 21:07:54 +0900

    refactor: move function to goal_controller

--- backend/controller/goal_controller.go
+++ backend/controller/goal_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -13,6 +14,33 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/oklog/ulid"
 )
+
+func FetchUserGoals(c *gin.Context) {
+
+	user_id := c.Param("user_id")
+	fmt.Println("c.Param", c.Param("user_id"))
+
+	goals := []model.Goal{}
+
+	rows, err := db.DB.Query("SELECT * FROM goals WHERE user_id = ?", user_id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var goal model.Goal
+		err = rows.Scan(&goal.ID, &goal.Title, &goal.Text, &goal.UserID, &goal.ImageURL, &goal.CreatedAt, &goal.UpdatedAt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		goals = append(goals, goal)
+	}
+
+	c.JSON(http.StatusOK, goals)
+}
 
 func AddGoal(c *gin.Context) {
 	t := time.Now()
