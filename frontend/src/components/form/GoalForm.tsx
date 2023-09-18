db074b2e1f570ee0d7f0273cf03ae7a20d8c9ed5
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-18 11:53:02 +0900

    feat: add new column

--- frontend/src/components/form/GoalForm.tsx
+++ frontend/src/components/form/GoalForm.tsx
@@ -2,28 +2,48 @@ import { Button, Container, Stack, TextField } from "@mui/material";
 import { RefObject } from "react";
 
 type GoalFormProps = {
-  setTitle: (title: string) => void;
-  setText: (text: string) => void;
+  // setTitle: (title: string) => void;
+  // setText: (text: string) => void;
+  SetSmartSpecific: (smart_specific: string) => void;
+  SetSmartMeasurable: (smart_measurable: string) => void;
+  SetSmartAchievable: (smart_achievable: string) => void;
+  SetSmartRelevant: (smart_relevant: string) => void;
+  SetSmartTimeBound: (smart_time_bound: string) => void;
   setFile: (file: File | null) => void;
   addGoal: () => void;
-  title: string;
-  text: string;
+  // title: string;
+  // text: string;
+  smart_specific: string;
+  smart_measurable: string;
+  smart_achievable: string;
+  smart_relevant: string;
+  smart_time_bound: string;
   inputRef: RefObject<HTMLInputElement>;
 };
 
 const GoalForm = ({
-  setTitle,
-  setText,
+  // setTitle,
+  // setText,
+  SetSmartSpecific,
+  SetSmartMeasurable,
+  SetSmartAchievable,
+  SetSmartRelevant,
+  SetSmartTimeBound,
   setFile,
   addGoal,
-  title,
-  text,
+  // title,
+  // text,
+  smart_specific,
+  smart_measurable,
+  smart_achievable,
+  smart_relevant,
+  smart_time_bound,
   inputRef,
 }: GoalFormProps) => {
   return (
     <Container sx={{ pt: 3 }}>
       <Stack spacing={2}>
-        <TextField
+        {/* <TextField
           label="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
@@ -32,6 +52,31 @@ const GoalForm = ({
           label="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
+        /> */}
+        <TextField
+          label="smart_specific"
+          value={smart_specific}
+          onChange={(e) => SetSmartSpecific(e.target.value)}
+        />
+        <TextField
+          label="smart_measurable"
+          value={smart_measurable}
+          onChange={(e) => SetSmartMeasurable(e.target.value)}
+        />
+        <TextField
+          label="smart_achievable"
+          value={smart_achievable}
+          onChange={(e) => SetSmartAchievable(e.target.value)}
+        />
+        <TextField
+          label="smart_relevant"
+          value={smart_relevant}
+          onChange={(e) => SetSmartRelevant(e.target.value)}
+        />
+        <TextField
+          label="smart_time_bound"
+          value={smart_time_bound}
+          onChange={(e) => SetSmartTimeBound(e.target.value)}
         />
         <input
           type="file"
