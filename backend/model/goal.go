60f3f2d609417203037f09092a549e64ee0365fd
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-19 21:51:53 +0900

    feat: add column to Goal struct

--- backend/model/goal.go
+++ backend/model/goal.go
@@ -3,11 +3,20 @@ package model
 import "time"
 
 type Goal struct {
-	ID string `json:"id"`
+	ID     string `json:"id"`
 	UserID string `json:"user_id"`
-	Title string `json:"title"`
-	Text string `json:"text"`
-	ImageURL *string `json:"image_url"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	// Title string `json:"title"`
+	// Text string `json:"text"`
+	ImageURL        *string   `json:"image_url"`
+	CreatedAt       time.Time `db:"created_at"`
+	UpdatedAt       time.Time `db:"updated_at"`
+	SmartSpecific   string    `json:"smart_specific"`
+	SmartMeasurable string    `json:"smart_measurable"`
+	SmartAchievable string    `json:"smart_achievable"`
+	SmartRelevant   string    `json:"smart_relevant"`
+	SmartTimeBound  string    `json:"smart_time_bound"`
+	Purpose         string    `json:"purpose"`
+	Loss            *string   `json:"loss"`
+	Phase           string    `json:"phase"`
+	Progress        int       `json:"progress"`
 }
