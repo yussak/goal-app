213243579ffcdded9e268d53d798cf4e683139a5
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-26 01:17:42 +0900

    refactor: rename

--- frontend/src/components/GoalList.tsx
+++ frontend/src/components/GoalList.tsx
@@ -17,11 +17,11 @@ const GoalList = ({ goals, onDelete }: GoalListProps) => {
         const isEdited = goal.CreatedAt !== goal.UpdatedAt;
         return (
           <li key={index} className="goal-list">
-            <p>specific: {goal.smart_specific}</p>
-            <p>measurable: {goal.smart_measurable}</p>
-            <p>achievable: {goal.smart_achievable}</p>
-            <p>relevant: {goal.smart_relevant}</p>
-            <p>time_bound: {goal.smart_time_bound}</p>
+            <p>specific: {goal.specific}</p>
+            <p>measurable: {goal.measurable}</p>
+            <p>achievable: {goal.achievable}</p>
+            <p>relevant: {goal.relevant}</p>
+            <p>time_bound: {goal.time_bound}</p>
             <p>purpose: {goal.purpose}</p>
             <p>loss: {goal.loss}</p>
             <p>progress: {goal.progress}</p>
