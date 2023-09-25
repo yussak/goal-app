497c30e19dcda9fce1736d3c0505f2e9fde2a90d
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-09-26 00:20:25 +0900

    refactor: remove unused migration file

--- backend/db/migrations/000008_rename_smart_columns.down.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE goals
-RENAME COLUMN 'specific' 'smart_specific',
-RENAME COLUMN 'measurable' 'smart_measurable',
-RENAME COLUMN 'achievable' 'smart_achievable',
-RENAME COLUMN 'relevant' 'smart_relevant',
-RENAME COLUMN 'time_bound' 'smart_time_bound';
--- backend/db/migrations/000008_rename_smart_columns.up.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE goals
-RENAME COLUMN `smart_specific` TO `specific`,
-RENAME COLUMN `smart_measurable` TO `measurable`,
-RENAME COLUMN `smart_achievable` TO `achievable`,
-RENAME COLUMN `smart_relevant` TO `relevant`,
-RENAME COLUMN `smart_time_bound` TO `time_bound`;
