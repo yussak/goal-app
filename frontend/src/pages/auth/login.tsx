f089b5107f4a7366da97dfe098b81e57d3ce7068
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-10-08 00:29:11 +0900

    docs: remove comment

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -2,10 +2,7 @@ import { useState } from "react";
 import LoginForm from "@/components/form/LoginForm";
 import { signIn } from "next-auth/react";
 
-// TODO:パスワード再登録可能にする→ https://github.com/YusukeSakuraba/goal-app/issues/27 で対応
 // TODO: バリデーション追加→空欄（requiredでできてそうだが揃えたい）、文字数・形式
-// TODO:ログイン成功時にフラッシュ出す
-// TODO:ログイン失敗時にフラッシュ出す
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
