9e60f6b26ed86df9cbc7514320cd5e997d1bab47
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-10-08 04:27:25 +0900

    feat: remove unnecessary text

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,13 +1,6 @@
-import { useSession } from "next-auth/react";
-
 export default function About() {
-  const { data: session } = useSession();
-
   return (
     <>
-      {/* TODO:nameが空文字の時もfalsyになるのでHelloが出てこない→正しいんだがそもそもnameが空欄にならぬようバリデーション追加する */}
-      {/* 一貫性を保つためpタグでもオプショナルチェーンで書いてる */}
-      {session?.user?.name && <p>Hello, {session?.user?.name}</p>}
       <div>about</div>
     </>
   );
