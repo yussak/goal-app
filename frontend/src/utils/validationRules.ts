90dad21d9c616c76966f704f67fa6cbcc69ae609
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-10-31 20:59:41 +0900

    feat: update goal validation

--- frontend/src/utils/validationRules.ts
+++ frontend/src/utils/validationRules.ts
@@ -1,5 +1,5 @@
 export const validationRules = {
   required: "必須です",
   minLength: { value: 3, message: "3文字以上入力してください" },
-  maxLength: { value: 5, message: "5文字以内で入力してください" },
+  maxLength: { value: 10, message: "10文字以内で入力してください" },
 };
