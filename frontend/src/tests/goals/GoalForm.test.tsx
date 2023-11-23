6045d8fe6919703d6c4e8675846a9b7b8e58a2b2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-11-23 19:51:08 +0900

    test: add test

--- frontend/src/tests/goals/GoalForm.test.tsx
+++ frontend/src/tests/goals/GoalForm.test.tsx
@@ -65,3 +65,31 @@ it("データを入力して送信できる", async () => {
     smartT: "asdf",
   });
 });
+
+it("正しくないデータを受け取てエラーを出せる", async () => {
+  render(
+    <GoalForm
+      SetGoalData={handleSetGoalDataMock}
+      goalData={{}}
+      addGoal={addGoalMock}
+    />
+  );
+  const user = userEvent.setup();
+
+  await user.type(
+    screen.getByRole("textbox", { name: "purpose" }),
+    "aaaaaaaaaaa"
+  );
+  await user.type(screen.getByRole("textbox", { name: "smartS" }), "a");
+
+  // 送信ボタンをクリック
+  await user.click(screen.getByRole("button", { name: "追加" }));
+
+  // フォームにdata-testidをつけて指定
+  expect(screen.getByTestId(`error-purpose`)).toHaveTextContent(
+    "10文字以内で入力してください"
+  );
+  expect(screen.getByTestId(`error-smartS`)).toHaveTextContent(
+    "3文字以上で入力してください"
+  );
+});
