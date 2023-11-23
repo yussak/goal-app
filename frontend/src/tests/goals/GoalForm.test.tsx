ae408c2f485d4d70a36c13da38991829d7e65d76
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-11-23 17:06:14 +0900

    refactor: fix test

--- frontend/src/tests/goals/GoalForm.test.tsx
+++ frontend/src/tests/goals/GoalForm.test.tsx
@@ -2,18 +2,27 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import GoalForm from "@/components/form/GoalForm";
 
-it("should render the basic fields", () => {
-  const goalData = {
-    purpose: "",
-    loss: "",
-    smartS: "",
-    smartM: "",
-    smartA: "",
-    smartR: "",
-    smartT: "",
-  };
+const handleSetGoalDataMock = vi.fn();
+const addGoalMock = vi.fn();
 
-  render(<GoalForm goalData={goalData} />);
+const goalData = {
+  purpose: "",
+  loss: "",
+  smartS: "",
+  smartM: "",
+  smartA: "",
+  smartR: "",
+  smartT: "",
+};
+
+it("フォームのテキストが表示できている", () => {
+  render(
+    <GoalForm
+      SetGoalData={handleSetGoalDataMock}
+      goalData={goalData}
+      addGoal={addGoalMock}
+    />
+  );
   expect(
     screen.getByText("達成したいことを書きましょう（必須）")
   ).toBeInTheDocument();
