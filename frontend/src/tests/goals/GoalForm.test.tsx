861393f3e0d3ac566ab503f2fba69bd3aaf2b72a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-11-23 17:49:27 +0900

    test: add test

--- frontend/src/tests/goals/GoalForm.test.tsx
+++ frontend/src/tests/goals/GoalForm.test.tsx
@@ -1,6 +1,9 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import GoalForm from "@/components/form/GoalForm";
+import userEvent from "@testing-library/user-event";
+
+// TODO: import候補が出ないのを修正
 
 const handleSetGoalDataMock = vi.fn();
 const addGoalMock = vi.fn();
@@ -15,7 +18,7 @@ const goalData = {
   smartT: "",
 };
 
-it("フォームのテキストが表示できている", () => {
+it("フォームのテキストが表示できている", async () => {
   render(
     <GoalForm
       SetGoalData={handleSetGoalDataMock}
@@ -26,4 +29,39 @@ it("フォームのテキストが表示できている", () => {
   expect(
     screen.getByText("達成したいことを書きましょう（必須）")
   ).toBeInTheDocument();
+});
+
+it("データを入力して送信できる", async () => {
+  render(
+    <GoalForm
+      SetGoalData={handleSetGoalDataMock}
+      goalData={{}}
+      addGoal={addGoalMock}
+    />
+  );
+  const user = userEvent.setup();
+
+  // 各フィールドに入力
+  await user.type(screen.getByRole("textbox", { name: "purpose" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "loss" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "smartS" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "smartM" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "smartA" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "smartR" }), "asdf");
+  await user.type(screen.getByRole("textbox", { name: "smartT" }), "asdf");
+
+  // 送信ボタンをクリック
+  await user.click(screen.getByRole("button", { name: "追加" }));
+
+  // addGoalがユーザーが入力したデータで呼び出されたことを確認
+  expect(addGoalMock).toBeCalledWith({
+    // TODO:重複しているのでリファクタしたい
+    purpose: "asdf",
+    loss: "asdf",
+    smartS: "asdf",
+    smartM: "asdf",
+    smartA: "asdf",
+    smartR: "asdf",
+    smartT: "asdf",
+  });
 });
