aac04983d2ca17e138ed652b1eb574cf9829616b
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-11-28 23:32:13 +0900

    docs: add comment

--- frontend/src/utils/validationRules.ts
+++ frontend/src/utils/validationRules.ts
@@ -1,4 +1,5 @@
 export const validationRules = {
+  // TODO:制限をDBに合わせる
   required: "必須です",
   minLength: { value: 3, message: "3文字以上で入力してください" },
   maxLength: { value: 10, message: "10文字以内で入力してください" },
