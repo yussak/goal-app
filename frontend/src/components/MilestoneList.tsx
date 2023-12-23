38c325debc3ca554021ba9af8ba095f5e05531b8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-12-23 14:08:06 +0900

    feat: add todo form

--- frontend/src/components/MilestoneList.tsx
+++ frontend/src/components/MilestoneList.tsx
@@ -6,6 +6,10 @@ import {
   Typography,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import TodoForm from "./form/TodoForm";
+import { useState } from "react";
+import { axios } from "@/utils/axios";
+import { useSession } from "next-auth/react";
 
 type MilestoneListProps = {
   milestones: Milestone[];
@@ -13,6 +17,24 @@ type MilestoneListProps = {
 };
 
 const MilestoneList = ({ milestones, onDelete }: MilestoneListProps) => {
+  const { data: session } = useSession();
+
+  const [todoContent, setTodoContent] = useState<string>("");
+
+  const addTodo = async (id: string) => {
+    const params = {
+      parent_id: id,
+      content: todoContent,
+      user_id: session?.user?.id,
+    };
+    try {
+      const res = await axios.post(`/goals/${id}/Todos`, params);
+      // await getTodos();
+      setTodoContent("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return milestones && milestones.length > 0 ? (
     <ul>
       {milestones.map((milestone, index) => {
@@ -31,6 +53,12 @@ const MilestoneList = ({ milestones, onDelete }: MilestoneListProps) => {
                 <Typography>
                   <p>id（デバッグ用）: {milestone.id}</p>
                   <p>goal_id（デバッグ用）: {milestone.goal_id}</p>
+                  <TodoForm
+                    setContent={setTodoContent}
+                    // milestone.idは親側でだけ必要。なので必要なところ（=ここ）でidを代入している
+                    addTodo={() => addTodo(milestone.id)}
+                    content={todoContent}
+                  />
                 </Typography>
               </AccordionDetails>
             </Accordion>
---
+++ frontend/src/components/form/TodoForm.tsx
@@ -0,0 +1,29 @@
+import { Box, Button, Stack, TextField } from "@mui/material";
+
+type TodoFormProps = {
+  setContent: (content: string) => void;
+  addTodo: () => void;
+  content: string;
+};
+
+const TodoForm = ({ setContent, addTodo, content }: TodoFormProps) => {
+  // TODO:react-hook-formで書き換える
+  return (
+    <Box component="form" noValidate autoComplete="off">
+      ちゅうもく
+      <Stack spacing={2} direction="row">
+        <TextField
+          id="outlined-read-only-input"
+          label="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <Button onClick={addTodo} variant="contained">
+          追加
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
+
+export default TodoForm;
