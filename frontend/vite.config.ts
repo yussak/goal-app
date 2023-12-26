c40ccd48b70d5b8529db06d5e99d5ac53a7c65b6
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2023-12-26 19:06:43 +0900

    feat: add setting for "react is not defined"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 /// <reference types="vitest" />
-
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
