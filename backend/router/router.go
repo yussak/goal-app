36b504720082c72350c453acce8f1128aabb6c9a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-04 20:05:35 +0900

    fix: fix path

--- backend/router/router.go
+++ backend/router/router.go
@@ -7,26 +7,26 @@ import (
 
 func SetupRouter(r *gin.Engine) {
 
-	r.GET("/api/healthcheck", controller.Healthcheck)
+	r.GET("/healthcheck", controller.Healthcheck)
 
-	r.POST("/api/auth/signup", controller.Signup)
-	r.POST("/api/auth/login", controller.Login)
-	r.POST("/api/auth/decodeToken", controller.DecodeToken)
-	r.POST("/api/auth/logout", controller.Logout)
+	r.POST("/auth/signup", controller.Signup)
+	r.POST("/auth/login", controller.Login)
+	r.POST("/auth/decodeToken", controller.DecodeToken)
+	r.POST("/auth/logout", controller.Logout)
 
-	r.GET("/api/:user_id/goals", controller.FetchUserGoals)
+	r.GET("/:user_id/goals", controller.FetchUserGoals)
 
-	r.POST("/api/goal", controller.AddGoal)
-	r.DELETE("/api/goal/:id", controller.DeleteGoal)
-	r.GET("/api/goals/:id", controller.FetchGoalDetails)
-	r.PUT("/api/goals/edit/:id", controller.EditGoal)
+	r.POST("/goal", controller.AddGoal)
+	r.DELETE("/goal/:id", controller.DeleteGoal)
+	r.GET("/goals/:id", controller.FetchGoalDetails)
+	r.PUT("/goals/edit/:id", controller.EditGoal)
 
-	r.POST("/api/goals/:id/milestones", controller.AddMilestone)
-	r.GET("/api/goals/:id/milestones", controller.FetchMilestones)
-	r.DELETE("/api/milestones/:id", controller.DeleteMilestone)
+	r.POST("/goals/:id/milestones", controller.AddMilestone)
+	r.GET("/goals/:id/milestones", controller.FetchMilestones)
+	r.DELETE("/milestones/:id", controller.DeleteMilestone)
 
-	r.POST("/api/milestones/:id/todos", controller.AddTodo)
-	r.GET("/api/milestones/:id/todos", controller.FetchTodos)
-	r.DELETE("/api/todos/:id", controller.DeleteTodo)
-	r.PUT("/api/todos/:id/is_completed", controller.UpdateTodoCheck)
+	r.POST("/milestones/:id/todos", controller.AddTodo)
+	r.GET("/milestones/:id/todos", controller.FetchTodos)
+	r.DELETE("/todos/:id", controller.DeleteTodo)
+	r.PUT("/todos/:id/is_completed", controller.UpdateTodoCheck)
 }
--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,5 +2,5 @@ import Axios from "axios";
 
 // todo:環境変数で書き換え必要そう
 export const axios = Axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/",
 });
