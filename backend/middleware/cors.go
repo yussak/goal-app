5bf6fda00b17d1b11dbb7a096e47f1d3849563f5
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-07 00:34:16 +0900

    fix: fix cors methods

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -8,7 +8,7 @@ func GetCorsConfig() cors.Config {
 	return cors.Config{
 		AllowOrigins: []string{"https://pf-goal-app.net", "http://localhost:3000"},
 
-		AllowMethods: []string{"POST, GET, OPTIONS, PUT, DELETE"},
+		AllowMethods: []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
 
 		AllowHeaders: []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
 
