f3da1fdcb8aa1977aa4817db58ad34d7299c18ec
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-08 18:23:42 +0900

    test: update test

--- frontend/src/tests/goals/GoalForm.test.tsx
+++ frontend/src/tests/goals/GoalForm.test.tsx
@@ -5,24 +5,14 @@ import userEvent from "@testing-library/user-event";
 import { GoalFormData } from "@/types";
 
 describe("GoalForm component", () => {
-  const SetGoalDataMock = vi.fn();
   const addGoalMock = vi.fn();
 
   afterEach(() => {
-    SetGoalDataMock.mockReset();
     addGoalMock.mockReset();
   });
 
   it("should submit when correct values are input", async () => {
-    expect.assertions(1);
-    render(
-      <GoalForm
-        SetGoalData={SetGoalDataMock}
-        // purpose:"",content:"",...と渡したら型エラーが出たため型を指定
-        goalData={{} as GoalFormData}
-        addGoal={addGoalMock}
-      />
-    );
+    render(<GoalForm addGoal={addGoalMock} />);
     const user = userEvent.setup();
 
     // 各フィールドに入力
@@ -47,107 +37,103 @@ describe("GoalForm component", () => {
   });
 
   it("should not submit when invalid values are input", async () => {
-    expect.assertions(1);
-    render(
-      <GoalForm
-        SetGoalData={SetGoalDataMock}
-        goalData={{} as GoalFormData}
-        addGoal={addGoalMock}
-      />
-    );
+    render(<GoalForm addGoal={addGoalMock} />);
     const user = userEvent.setup();
 
+    // contentは空
     await user.type(screen.getByRole("textbox", { name: "purpose" }), "目的");
     await user.type(
       screen.getByRole("textbox", { name: "loss" }),
       "ロスですロスですロスです"
     );
 
-    await user.click(screen.getByRole("button", { name: "追加" }));
+    expect(screen.getByRole("button", { name: "追加" })).toBeDisabled();
     expect(addGoalMock).not.toBeCalled();
   });
 
-  // it("should validate when values are not input", async () => {
-  //   expect.assertions(3);
-  //   render(
-  //     <GoalForm
-  //       SetGoalData={SetGoalDataMock}
-  //       goalData={{} as GoalFormData}
-  //       addGoal={addGoalMock}
-  //     />
-  //   );
-  //   const user = userEvent.setup();
+  it("should validate when too short values are input", async () => {
+    render(<GoalForm addGoal={addGoalMock} />);
+    const user = userEvent.setup();
 
-  //   // 値を入力せず送信ボタンをクリック
-  //   await user.click(screen.getByRole("button", { name: "追加" }));
+    await user.type(screen.getByRole("textbox", { name: "purpose" }), "目的");
+    await user.type(screen.getByRole("textbox", { name: "loss" }), "ロス");
+    await user.type(screen.getByRole("textbox", { name: "content" }), "内容");
+    await user.click(screen.getByRole("button", { name: "追加" }));
 
-  //   expect(screen.getByTestId(`error-purpose`)).toHaveTextContent("必須です");
-  //   expect(screen.getByTestId(`error-content`)).toHaveTextContent("必須です");
-  //   expect(screen.getByTestId(`error-loss`)).toHaveTextContent("必須です");
-  // });
+    // ボタンクリックはできる
+    expect(screen.getByRole("button", { name: "追加" })).not.toBeDisabled();
+    expect(screen.getByTestId(`error-purpose`)).toHaveTextContent(
+      "3文字以上で入力してください"
+    );
+    expect(screen.getByTestId(`error-content`)).toHaveTextContent(
+      "3文字以上で入力してください"
+    );
+    expect(screen.getByTestId(`error-loss`)).toHaveTextContent(
+      "3文字以上で入力してください"
+    );
+  });
 
-  // it("should validate when too short values are input", async () => {
-  //   expect.assertions(3);
-  //   render(
-  //     <GoalForm
-  //       SetGoalData={SetGoalDataMock}
-  //       goalData={{} as GoalFormData}
-  //       addGoal={addGoalMock}
-  //     />
-  //   );
-  //   const user = userEvent.setup();
+  it("should validate when too long values are input", async () => {
+    render(<GoalForm addGoal={addGoalMock} />);
+    const user = userEvent.setup();
 
-  //   await user.type(screen.getByRole("textbox", { name: "purpose" }), "目的");
-  //   await user.type(screen.getByRole("textbox", { name: "loss" }), "ロス");
-  //   await user.type(screen.getByRole("textbox", { name: "content" }), "内容");
+    await user.type(
+      screen.getByRole("textbox", { name: "purpose" }),
+      "サンプルテキストサンプルテキスト"
+    );
+    await user.type(
+      screen.getByRole("textbox", { name: "loss" }),
+      "サンプルテキストサンプルテキスト"
+    );
+    await user.type(
+      screen.getByRole("textbox", { name: "content" }),
+      "サンプルテキストサンプルテキスト"
+    );
+    await user.click(screen.getByRole("button", { name: "追加" }));
 
-  //   await user.click(screen.getByRole("button", { name: "追加" }));
+    expect(screen.getByRole("button", { name: "追加" })).not.toBeDisabled();
 
-  //   expect(screen.getByTestId(`error-purpose`)).toHaveTextContent(
-  //     "3文字以上で入力してください"
-  //   );
-  //   expect(screen.getByTestId(`error-content`)).toHaveTextContent(
-  //     "3文字以上で入力してください"
-  //   );
-  //   expect(screen.getByTestId(`error-loss`)).toHaveTextContent(
-  //     "3文字以上で入力してください"
-  //   );
-  // });
+    expect(screen.getByTestId(`error-purpose`)).toHaveTextContent(
+      "10文字以内で入力してください"
+    );
+    expect(screen.getByTestId(`error-content`)).toHaveTextContent(
+      "10文字以内で入力してください"
+    );
+    expect(screen.getByTestId(`error-loss`)).toHaveTextContent(
+      "10文字以内で入力してください"
+    );
+  });
 
-  // it("should validate when too long values are input", async () => {
-  //   expect.assertions(3);
-  //   render(
-  //     <GoalForm
-  //       SetGoalData={SetGoalDataMock}
-  //       goalData={{} as GoalFormData}
-  //       addGoal={addGoalMock}
-  //     />
-  //   );
-  //   const user = userEvent.setup();
+  it("should submit when invalid values are fixed", async () => {
+    render(<GoalForm addGoal={addGoalMock} />);
+    const user = userEvent.setup();
 
-  //   await user.type(
-  //     screen.getByRole("textbox", { name: "purpose" }),
-  //     "サンプルテキストサンプルテキスト"
-  //   );
-  //   await user.type(
-  //     screen.getByRole("textbox", { name: "loss" }),
-  //     "サンプルテキストサンプルテキスト"
-  //   );
-  //   await user.type(
-  //     screen.getByRole("textbox", { name: "content" }),
-  //     "サンプルテキストサンプルテキスト"
-  //   );
+    // バリデーションエラーになる値を入力
+    // contentは空
+    await user.type(screen.getByRole("textbox", { name: "purpose" }), "目的");
+    await user.type(
+      screen.getByRole("textbox", { name: "loss" }),
+      "ロスですロスですロスです"
+    );
 
-  //   await user.click(screen.getByRole("button", { name: "追加" }));
+    expect(screen.getByRole("button", { name: "追加" })).toBeDisabled();
 
-  //   expect(screen.getByTestId(`error-purpose`)).toHaveTextContent(
-  //     "10文字以内で入力してください"
-  //   );
-  //   expect(screen.getByTestId(`error-content`)).toHaveTextContent(
-  //     "10文字以内で入力してください"
-  //   );
-  //   expect(screen.getByTestId(`error-loss`)).toHaveTextContent(
-  //     "10文字以内で入力してください"
-  //   );
-  // });
+    // リセットして正しい値を入力し直す
+    await user.clear(screen.getByRole("textbox", { name: "purpose" }));
+    await user.clear(screen.getByRole("textbox", { name: "content" }));
+    await user.clear(screen.getByRole("textbox", { name: "loss" }));
+
+    await user.type(screen.getByRole("textbox", { name: "purpose" }), "テスト");
+    await user.type(screen.getByRole("textbox", { name: "content" }), "テスト");
+    await user.type(screen.getByRole("textbox", { name: "loss" }), "テスト");
+
+    expect(screen.getByRole("button", { name: "追加" })).not.toBeDisabled();
+    await user.click(screen.getByRole("button", { name: "追加" }));
+
+    expect(addGoalMock).toBeCalledWith({
+      purpose: "テスト",
+      content: "テスト",
+      loss: "テスト",
+    });
+  });
 });
