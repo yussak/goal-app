f5014f1f5bd36bebaf79570b6b7af0eec4b8675e
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-12 19:12:25 +0900

    docs: add comment

--- frontend/src/utils/checkAuth.ts
+++ frontend/src/utils/checkAuth.ts
@@ -6,6 +6,7 @@ export function checkAuth() {
   const { data: session, status } = useSession();
 
   // todo:ローディング画面実装→app routerでやりたい
+  // todo:以下だと中身が見えるので対応→そのためにもローディングが必要かもしれない
   useEffect(() => {
     if (status === "loading") return;
     if (!session) {
