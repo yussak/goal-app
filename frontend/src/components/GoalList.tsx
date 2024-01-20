123ab495a20175d7a0b8c888f5e03bfe496602b0
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-20 21:46:39 +0900

    refactor: separate file

--- frontend/src/components/GoalList.tsx
+++ frontend/src/components/GoalList.tsx
@@ -1,87 +1,12 @@
 import Link from "next/link";
 import { Goal } from "@/types";
 import { useSession } from "next-auth/react";
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Typography,
-} from "@mui/material";
-import { useState } from "react";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { SimpleDialog } from "./DeleteDialog";
 
 type GoalListProps = {
   goals: Goal[];
   onDelete: (id: string) => void;
 };
-
-export type SimpleDialogProps = {
-  selectedValue: Goal | null;
-  onDelete: (id: string) => void;
-};
-
-function SimpleDialog(props: SimpleDialogProps) {
-  const [open, setOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState<Goal | null>(null);
-
-  const handleClickOpen = (value: Goal | null) => {
-    setSelectedValue(value);
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleDelete = async () => {
-    if (selectedValue && selectedValue.id) {
-      try {
-        await props.onDelete(selectedValue?.id);
-        setOpen(false);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
-
-  return (
-    <>
-      <Button
-        variant="outlined"
-        onClick={() => handleClickOpen(props?.selectedValue)}
-        startIcon={<DeleteOutlineIcon />}
-      >
-        目標を削除
-      </Button>
-      <Dialog onClose={handleClose} open={open}>
-        <DialogTitle>目標を削除してよろしいですか？</DialogTitle>
-        <DialogContent>
-          <Typography gutterBottom>
-            目標を削除すると、それに紐づくマイルストーン、TODOも削除されます。
-          </Typography>
-          {/* <Typography gutterBottom>デバッグ用：{selectedValue?.id}</Typography>
-          <Typography gutterBottom>
-            デバッグ用：{selectedValue?.content}
-          </Typography> */}
-        </DialogContent>
-        <DialogActions>
-          <Button color="error" onClick={handleDelete}>
-            目標を削除
-          </Button>
-          <Button
-            onClick={() => {
-              setOpen(false);
-            }}
-          >
-            cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
-}
 
 const GoalList = ({ goals, onDelete }: GoalListProps) => {
   const { data: session } = useSession();
