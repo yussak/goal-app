4d79d1207f2332897e37237b52dfec3cee678a7c
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-20 20:38:12 +0900

    fix: fix to create overlay one time

--- frontend/src/components/GoalList.tsx
+++ frontend/src/components/GoalList.tsx
@@ -23,66 +23,94 @@ export type SimpleDialogProps = {
 };
 
 function SimpleDialog(props: SimpleDialogProps) {
-  const { onClose, selectedValue, open } = props;
+  // const { onClose, selectedValue, open } = props;
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState<Goal | null>(null);
+
+  // const handleClickOpen = () => {
+  const handleClickOpen = (value: Goal | null) => {
+    setSelectedValue(value);
+    setOpen(true);
+  };
+
+  // const handleClose = () => {
+  //   // setOpen(false);
+  // };
 
   const handleClose = () => {
-    onClose(selectedValue);
+    setOpen(false);
+    // onClose(selectedValue);
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Set backup account</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        <ListItem disableGutters>
-          <ListItemButton autoFocus>{selectedValue?.id}</ListItemButton>
-        </ListItem>
-      </List>
-    </Dialog>
+    <>
+      {/* <Button variant="outlined" onClick={() => handleClickOpen()}> */}
+      <Button
+        variant="outlined"
+        onClick={() => handleClickOpen(props?.selectedValue)}
+      >
+        {/* <Button variant="outlined" onClick={() => handleClickOpen(goal)}> */}
+        Open simple dialog
+      </Button>
+      <Dialog onClose={handleClose} open={open}>
+        <DialogTitle>Set backup account</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          <ListItem disableGutters>
+            <ListItemButton autoFocus>{selectedValue?.id}</ListItemButton>
+          </ListItem>
+          <ListItem disableGutters>
+            <ListItemButton autoFocus>{selectedValue?.content}</ListItemButton>
+          </ListItem>
+        </List>
+      </Dialog>
+    </>
   );
 }
 
 const GoalList = ({ goals, onDelete }: GoalListProps) => {
   const { data: session } = useSession();
-  const [open, setOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState<Goal | null>(null);
+  // const [open, setOpen] = useState(false);
+  // const [selectedValue, setSelectedValue] = useState<Goal | null>(null);
 
-  const handleClickOpen = (value: Goal) => {
-    setSelectedValue(value);
-    setOpen(true);
-  };
+  // const handleClickOpen = (value: Goal) => {
+  //   setSelectedValue(value);
+  //   setOpen(true);
+  // };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
 
   return goals && goals.length > 0 ? (
     <ul>
       {goals.map((goal, index) => {
         return (
           <li key={index} className="goal-list">
-            <p>content: {goal.content}</p>
+            {/* <p>content: {goal.content}</p>
             <p>purpose: {goal.purpose}</p>
             <p>loss: {goal.loss}</p>
-            <p>progress: {goal.progress}</p>
+            <p>progress: {goal.progress}</p> */}
             <p>id: {goal.id}</p>
-            <p>user_id（デバッグ用）: {goal.user_id}</p>
+            {/* <p>user_id（デバッグ用）: {goal.user_id}</p>
             <p>CreatedAt: {goal.CreatedAt.toLocaleString()}</p>
-            <p>UpdatedAt: {goal.UpdatedAt.toLocaleString()}</p>
+            <p>UpdatedAt: {goal.UpdatedAt.toLocaleString()}</p> */}
             <p>
               <Link href={`/goals/${goal.id}`}>detail</Link>
             </p>
             {session?.user?.id === goal.user_id ? (
               <p>
-                <Button
+                {/* <Button
                   variant="outlined"
                   onClick={() => handleClickOpen(goal)}
                 >
                   Open simple dialog
-                </Button>
+                </Button> */}
                 <SimpleDialog
-                  selectedValue={selectedValue}
-                  open={open}
-                  onClose={handleClose}
+                  selectedValue={goal}
+                  // selectedValue={selectedValue}
+                  // open={open}
+                  // onClose={handleClose}
+                  // onDelete={onDelete}
                 />
                 {/* <button onClick={() => onDelete(goal.id)}>delete</button> */}
               </p>
