37a75b3ac470dc3eb90413f4a066814c8f5b6a43
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-27 23:23:20 +0900

    test: add test

--- frontend/src/tests/unit/goals/create.test.tsx
+++ frontend/src/tests/unit/goals/create.test.tsx
@@ -2,17 +2,28 @@ import CreateGoal from "@/pages/goals/create";
 import { render } from "@testing-library/react";
 import { SessionProvider } from "next-auth/react";
 
-describe("goals/create.tsx", () => {
-  const mockSession = {
-    expires: new Date(Date.now() + 2 * 86400).toISOString(),
-    user: { name: "test", email: "test@test.com", id: "testId" },
+let mockRouter;
+beforeEach(() => {
+  mockRouter = {
+    asPath: "/goals/create",
+    push: vi.fn(),
   };
+
   vi.mock("next/router", () => ({
-    useRouter: () => ({
-      asPath: "/goals/create",
-    }),
+    useRouter: () => mockRouter,
   }));
+});
+
+afterEach(() => {
+  vi.resetAllMocks();
+});
+
+describe("goals/create.tsx", () => {
   it("should get user id if session is not null", () => {
+    const mockSession = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: { name: "test", email: "test@test.com", id: "testId" },
+    };
     render(
       <SessionProvider session={mockSession}>
         <CreateGoal />
@@ -20,5 +31,15 @@ describe("goals/create.tsx", () => {
     );
 
     expect(mockSession?.user.id).toBe("testId");
+  });
+
+  it("should redirect to login page if session is null", () => {
+    render(
+      <SessionProvider session={null}>
+        <CreateGoal />
+      </SessionProvider>
+    );
+
+    expect(mockRouter.push).toHaveBeenCalledWith("/auth/login");
   });
 });
