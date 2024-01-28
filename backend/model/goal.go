e32d7dbcf9ae846b18c34c2ede4922298d69f8f2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-28 11:51:26 +0900

    refactor: update to camelCase

--- backend/model/goal.go
+++ backend/model/goal.go
@@ -3,11 +3,10 @@ package model
 import "time"
 
 type Goal struct {
-	// todo:json:の方キャメルにあわせる（フロントで使うが、フロントはキャメルケースのため）
 	ID        string    `json:"id"`
 	UserID    string    `json:"userId"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	CreatedAt time.Time `db:"createdAt"`
+	UpdatedAt time.Time `db:"updatedAt"`
 	Content   string    `json:"content"`
 	Purpose   string    `json:"purpose"`
 	Loss      *string   `json:"loss"`
