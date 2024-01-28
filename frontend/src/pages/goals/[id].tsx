64ebeb56304f62843b433b31b594ab4da719985c
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-28 12:50:13 +0900

    docs: fix i18n text

--- frontend/src/pages/goals/[id].tsx
+++ frontend/src/pages/goals/[id].tsx
@@ -123,12 +123,12 @@ const GoalDetail: CustomNextPage = () => {
 
   return (
     <>
-      <h2>{t("goal_edit.title1")}</h2>
+      <h2>{t("goal_detail.title1")}</h2>
       {goal && (
         <>
           {/* todo:このページでも削除可能にしたい */}
           <Button href={`/goals/edit/${goal.id}`} startIcon={<EditIcon />}>
-            {t("goal_edit.button1")}
+            {t("goal_detail.button1")}
           </Button>
           <p>content: {goal.content}</p>
           <p>purpose: {goal.purpose}</p>
@@ -138,20 +138,20 @@ const GoalDetail: CustomNextPage = () => {
           {/* <p>userId(デバッグ用): {goal.userId}</p> */}
           {/* <p>CreatedAt(デバッグ用): {goal.CreatedAt.toString()}</p> */}
           {/* <p>UpdatedAt(デバッグ用): {goal.UpdatedAt.toString()}</p> */}
-          <Link href="/goals">{t("goal_edit.link1")}</Link>
+          <Link href="/goals">{t("goal_detail.link1")}</Link>
         </>
       )}
       {session?.user && (
         <>
-          <h3>{t("goal_edit.title2")}</h3>
+          <h3>{t("goal_detail.title2")}</h3>
           {milestones.length < 5 ? (
             <MilestoneForm addMilestone={addMilestone} />
           ) : (
-            <p>{t("goal_edit.text1")}</p>
+            <p>{t("goal_detail.text1")}</p>
           )}
         </>
       )}
-      <h3>{t("goal_edit.title3")}</h3>
+      <h3>{t("goal_detail.title3")}</h3>
       <MilestoneList
         milestones={milestones}
         onDeleteMilestone={deleteMilestone}
