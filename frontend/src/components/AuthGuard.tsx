b246df24b5f28b6b078227caf8ba13259a8490e8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-28 11:42:17 +0900

    refactor: fix comment

--- frontend/src/components/AuthGuard.tsx
+++ frontend/src/components/AuthGuard.tsx
@@ -6,11 +6,8 @@ const AuthGuard = ({ children }: { children: ReactNode }): any => {
   const { status } = useSession();
   const router = useRouter();
 
-  // todo:ローディング画面実装→app routerでやりたい
   // todo:以下だと中身が見えるので対応→そのためにもローディングが必要かもしれない
   useEffect(() => {
-    // ログイン済の時はリダイレクトしない
-    // if (status === "loading") return;
     if (
       // 未ログインで認証が必要なページにアクセスしたらログインページにリダイレクトする
       // ログインまたは新規登録ページの時はリダイレクトしない
@@ -20,16 +17,9 @@ const AuthGuard = ({ children }: { children: ReactNode }): any => {
     ) {
       router.push("/auth/login");
     }
-    // if (
-    //   // ログイン済でログインまたは新規登録ページにアクセスしたらトップページにリダイレクトする
-    //   status === "authenticated" &&
-    //   (router.pathname === "/auth/login" || router.pathname === "/auth/signup")
-    // ) {
-    //   router.push("/");
-    // }
   }, [router, status]);
   if (status === "loading") return <p> loading</p>;
-  // ログイン済でログインまたは新規登録ページにアクセスしたらトップページにリダイレクトする
+  // ログイン済の時はchildrenを表示する
   if (status === "authenticated") {
     return children;
   }
