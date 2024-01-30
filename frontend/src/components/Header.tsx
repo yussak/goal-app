23f07e8feb01b1d5b86edfd4de076c0763329f7d
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-31 00:33:08 +0900

    feat: remove home link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
 import { useSession } from "next-auth/react";
-import HomeIcon from "@mui/icons-material/Home";
 
 export default function Header() {
   const { data: session } = useSession();
 
   return (
     <header className="header">
-      <Link href="/">
-        <HomeIcon />
-      </Link>
       {session?.user?.name && <p>Login as {session?.user?.name}</p>}
     </header>
   );
