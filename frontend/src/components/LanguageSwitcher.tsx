bd24b0735f7c9e433becc995fd9efc4e71f70f9a
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-01-30 23:50:12 +0900

    style: use mui select

--- frontend/src/components/LanguageSwitcher.tsx
+++ frontend/src/components/LanguageSwitcher.tsx
@@ -1,6 +1,6 @@
+import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
-import { ChangeEvent } from "react";
 
 type Language = "ja" | "en";
 
@@ -8,16 +8,18 @@ export const LanguageSwitcher: React.FC = () => {
   const { t, i18n } = useTranslation();
   const router = useRouter();
 
-  const changeLanguage = async (event: ChangeEvent<HTMLSelectElement>) => {
+  const changeLanguage = async (event: SelectChangeEvent<string>) => {
     const newLocale = event.target.value as Language;
     await i18n.changeLanguage(newLocale);
     await router.push(router.asPath, router.asPath, { locale: newLocale });
   };
 
   return (
-    <select defaultValue={i18n.language} onChange={changeLanguage}>
-      <option value="ja">{t("japanese")}</option>
-      <option value="en">{t("english")}</option>
-    </select>
+    <>
+      <Select defaultValue={i18n.language} onChange={changeLanguage}>
+        <MenuItem value="ja">{t("japanese")}</MenuItem>
+        <MenuItem value="en">{t("english")}</MenuItem>
+      </Select>
+    </>
   );
 };
