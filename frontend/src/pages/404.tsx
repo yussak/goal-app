b868d12fbf7d6693fa7a33b62e7282534c0a8232
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-02-02 21:46:26 +0900

    feat: update 404

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,7 +1,17 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function Custom404() {
   return (
-    <>
+    <div className="not-found">
       <h1>404 - Page Not Found</h1>
-    </>
+    </div>
   );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
