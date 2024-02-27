9011eef3ee101389039accd0177d65afe4f83386
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-02-27 22:17:36 +0900

    refactor: remove unused code

--- backend/main_test.go
+++ backend/main_test.go
@@ -9,35 +9,6 @@ import (
 	"time"
 )
 
-// func add(a, b int) int {
-// 	return a + b
-// }
-
-// func TestAdd(t *testing.T) {
-// 	type args struct {
-// 		a int
-// 		b int
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want int
-// 	}{
-// 		{
-// 			name: "normal",
-// 			args: args{a: 1, b: 2},
-// 			want: 3,
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			if got := add(tt.args.a, tt.args.b); got != tt.want {
-// 				t.Errorf("add() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
-
 func TestMain(m *testing.M) {
 	// テスト用DBへの接続初期化
 	_, err := InitTestDB() // 環境変数などでテスト用DB情報を設定
