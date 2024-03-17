887bdea9f2c5c4d84de41f317ad3ec12483964a5
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-03-17 09:50:39 +0900

    feat: update struct

--- backend/model/goal.go
+++ backend/model/goal.go
@@ -8,7 +8,7 @@ type Goal struct {
 	CreatedAt time.Time `db:"createdAt"`
 	UpdatedAt time.Time `db:"updatedAt"`
 	Content   string    `json:"content"`
-	Purpose   string    `json:"purpose"`
+	Purpose   *string   `json:"purpose"`
 	Benefit   *string   `json:"benefit"`
 	Phase     string    `json:"phase"`
 	Progress  int       `json:"progress"`
