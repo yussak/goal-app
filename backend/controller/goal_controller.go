5157819fef1f060ebe9f3d49d144379df14bb019
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-03-21 23:30:41 +0900

    refactor: remove redundant return

--- backend/controller/goal_controller.go
+++ backend/controller/goal_controller.go
@@ -78,7 +78,6 @@ func AddGoal(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, req)
-	return
 }
 
 func DeleteGoal(c *gin.Context) {
@@ -188,5 +187,4 @@ func EditGoal(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, goal)
-	return
 }
